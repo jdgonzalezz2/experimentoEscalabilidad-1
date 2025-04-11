2286b00c640883a699a00d28441e55d771241ef8
Julian Gonzalez <jd.gonzalez2@uniandes.edu.co>
2025-04-11 08:49:51 -0500

    3214

--- experimentoEscalabilidad/urls.py
+++ experimentoEscalabilidad/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/enviar-diagnostico/', resultados_views.enviar_diagnostico),
 ]
--- gestor_resultados/urls.py
+++ gestor_resultados/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('diagnostico/', views.diagnostico_view, name='diagnostico'),
+    path('api/enviar-diagnostico/',views.enviar_diagnostico)
 ] 
