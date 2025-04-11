f1fec7d98618381c2784d1660553cf9f43b859da
Julian Gonzalez <jd.gonzalez2@uniandes.edu.co>
2025-04-11 08:33:23 -0500

    jjjj

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from gestor_examenes import views as examenes_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/examenes/', include('gestor_examenes.urls')),
     path('api/resultados/', include('gestor_resultados.urls')),
-    path('api/enviar-diagnostico/', views.enviar_diagnostico)
+    path('api/enviar-diagnostico/', examenes_views.enviar_diagnostico)
 ] 
